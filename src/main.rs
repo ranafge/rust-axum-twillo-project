a414a842fa2a564459ef5a9f753c5de1987932d3
ranafge <ranafge@gmail.com>
2024-02-23 16:27:32 +0600

    axum error handler added

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use axum::{body::{Body, HttpBody}, http::{Response, StatusCode}, response::IntoResponse, routing::{get, post, put}, Json, Router};
+use axum::{body::{Body, HttpBody}, extract::Path, http::{Response, StatusCode}, response::{Html, IntoResponse}, routing::{delete, get, post, put}, Json, Router};
 use tokio::net::TcpListener;
 use serde::Serialize;
 
@@ -21,9 +21,10 @@ async fn create_user() -> impl IntoResponse {
 
 // endpoint /users
 async fn list_users() -> Json<Vec<User>> {
+    println!("list user handler");
     let users = vec![
         User {
-            id:1,
+            id:2,
             name: "Elijah".to_string(),
             email: "Elijah@doe.com".to_string()
         },
@@ -36,12 +37,41 @@ async fn list_users() -> Json<Vec<User>> {
     Json(users)
 }
 
+
+// Error handaling 
+
+
+async fn delete_user(Path(user_id):Path<u64>) -> Result<Json<User>, impl IntoResponse> {
+    println!("delete_user handler");
+    match perform_delete_user(user_id).await {
+        Ok(_) => Ok(Json(User {
+            id: user_id,
+            name: "Elijah".to_string(),
+            email: "Elijah@doe.com".to_string()
+        })),
+        Err(e) => Err((
+            StatusCode::INTERNAL_SERVER_ERROR,
+            Html(format!("Failed to delete user: {}", e))
+        ))
+    }
+}
+
+async fn perform_delete_user(user_id: u64) -> Result<(), String> {
+    if user_id == 1 {
+        Err("User cannot be deleted".to_string())
+    }else {
+        // logic to delete a user ...
+        Ok(())
+    }
+}
+
 #[tokio::main]
 async fn main() {
     let app = Router::new().route("/", get(|| async { "Hello, Rust!" }))
             .route("/create-user", post(create_user))
             .route("/create-user", put(create_user))
             .route("/user", get(list_users))
+            .route("/delete-user/:user_id", delete(delete_user))
     ;
 
     let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
